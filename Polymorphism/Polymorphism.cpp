46c4714d8907d2e0985ddeda586e2ca60e413f79
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-31 16:36:33 +0700

    Memanggil Class dan menghasilkan output yang sesuai

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -24,9 +24,25 @@ public:
 
 };
 
-class Lia :public seseorang {
+class lia :public seseorang {
 public:
 	void pesan() {
 		cout << "Pesan dari Lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* orang;
+	joko a;
+	lia b;
+
+	orang = &a;
+	orang->pesan();
+	orang = &b;
+	orang->pesan();
+	//a.seseorang::pesan(); //memanggil method yang berada di class orang tua
+	//b.sesrorang::pesan();
+
+
+	return 0;
+}
