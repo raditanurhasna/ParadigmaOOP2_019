1927d63795404cf06db2941935c1b0affa0d6b61
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-31 16:47:52 +0700

    Membuat Class 'baseClass' dan turnananya 'derivedClass'

---
+++ noOverriding/noOverriding.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+	virtual void perkenalan() final {
+		cout << "Hallo saya function dari base class ";
+	}
+};
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya function dari derived class ";
+	}
+};
