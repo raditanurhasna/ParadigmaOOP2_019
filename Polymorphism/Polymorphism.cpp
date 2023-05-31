4fdaf49d4f21f4fce02b1c6ef28f23f6b37205e9
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-31 16:31:44 +0700

    Membuat Class seseorang dan Membuat Class joko, Lia yang merupakan turunan dari class seseorang.

---
+++ Polymorphism/Polymorphism.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+	virtual void pesan() = 0;
+	//virtual void pesan() {
+		//cout<< "pesan dari seseorang" endl;
+	//}
+	//virtual void cetak() = 0;
+	virtual void cetak() {
+
+	}
+};
+
+class joko :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari joko" << endl;
+	}
+	void cetak() {
+		cout << "Tulisan Joko" << endl;
+	}
+
+};
+
+class Lia :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari Lia" << endl;
+	}
+};
